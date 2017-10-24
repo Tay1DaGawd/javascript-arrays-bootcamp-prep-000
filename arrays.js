6c026061795c23b736aee0eac2d8146ef07c0b57
Tashan O'Neal <tdoneal91@gmail.com>
2017-10-24 13:03:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,3 +29,8 @@ function removeElementFromBeginningOfArray(array) {
   var array = [1, 2, 3]
   return array.slice(1)
 }
+function destructivelyRemoveElementFromEndOfArray(array) {
+  var array = [1, 2, 3]
+  array.pop()
+  return array
+}
